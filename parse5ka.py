129fe7b117ce8fb9d978b6e7640efbbc3f177e2a
AVSokolov <aleksander.sokolov@gmail.com>
2020-12-26 18:04:02 +0300

    c кат

--- parse5ka.py
+++ parse5ka.py
@@ -1,3 +1,4 @@
+import os
 import time
 import json
 from pathlib import Path
@@ -43,7 +44,8 @@ class Parser5ka:
         'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.16; rv:84.0) Gecko/20100101 Firefox/84.0"
     }
 
-    def __init__(self, start_url):
+    def __init__(self, start_url, category):
+        self.category = category
         self.start_url = start_url
 
     def _get_response(self, url, **kwargs):
@@ -59,9 +61,11 @@ class Parser5ka:
 
     def run(self):
         for products in self.parse(self.start_url):
-            for product in products:
-                file_path = Path(__file__).parent.joinpath(f'{product["id"]}.json')
-                self.save_file(file_path, product)
+            #for product in products:
+            #    file_path = Path(__file__).parent.joinpath("json").joinpath(f'{product["id"]}.json')
+            #    self.save_file(file_path, product)
+            file_path = Path(__file__).parent.joinpath("json").joinpath(self.category +'.json')
+            self.save_file(file_path, products)
 
     def parse(self, url):
         while url:
@@ -70,12 +74,26 @@ class Parser5ka:
             url = data['next']
             yield data.get('results', [])
 
+    def parseCat(self, url):
+        while url:
+            response = self._get_response(url, headers=self.headers)
+            data: dict = response.json()
+            yield data
+
     def save_file(self, file_path: Path, data: dict):
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
         with open(file_path, 'w', encoding='UTF-8') as file:
             # file.write(json.dumps(data))
             json.dump(data, file, ensure_ascii=False)
 
 
 if __name__ == '__main__':
-    parser = Parser5ka('https://5ka.ru/api/v2/special_offers/')
-    parser.run()
+
+    parserCat = Parser5ka('https://5ka.ru/api/v2/categories/', '0')
+    categories1 = parserCat.parseCat('https://5ka.ru/api/v2/categories/')
+
+    for categories in categories1:
+        for cat in categories:
+            parser = Parser5ka('https://5ka.ru/api/v2/special_offers/?categories=' + cat.get('parent_group_code'), cat.get('parent_group_code'))
+            # parser = Parser5ka('https://5ka.ru/api/v2/special_offers/')
+            parser.run()
